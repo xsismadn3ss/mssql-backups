9a7a51ceea88809c9901a6d32069005092879fb8
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-04-30 08:15:17 -0600

    🔥 Eliminar funcion para ejecutar comando sql en el contendor

--- utils/container.py
+++ utils/container.py
@@ -17,21 +17,3 @@ def list_container_files(path: str):
         return exec_result.output.decode()
     except Exception as e:
         print(f"Error al listar archivos en el contenedor: {e}")
-
-
-def execute_sql_in_container(
-    container_config: ContainerConfig, db_config: DbConfig, sql_command: str
-):
-    container: Container = get_container(container_config)
-
-    try:
-        exec_result = container.exec_run(
-            f'/opt/mssql-tools/bin/sqlcmd \
-                -S {db_config.host} \
-                -U {db_config.user} \
-                -P {db_config.password} \
-                -Q "{sql_command}"'
-        )
-        return exec_result.output.decode()
-    except Exception as e:
-        print(f"Error al ejecutar comando SQL en el contenedor: {e}")
