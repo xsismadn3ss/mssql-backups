894c87169476c9d59629e63168ca91ef13965a82
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-04-30 11:54:08 -0600

    ✨ Actualizar funcion main
    Agregar funcion de correr aplicacion

--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-from config.app import DbConfig, ContainerConfig
-from utils.container import list_container_files
-from constants.restore_db import SQL_COMMAND
-from constants.test_db import SQL_COMMAND as TEST_SQL_COMMAND
+from service.app_service import run_app
+
 
 def main():
-    DbConfig.validate()  # validar configuración de la base de datos
-    ContainerConfig.validate()
-
-    # Listar archivos en el directorio de backup del contenedor
-    backups: str = list_container_files(ContainerConfig.backup_dir)  # type: ignore
-    files = backups.splitlines()
-    return
-
-    for file in files:
-        backup_path = f"{ContainerConfig.backup_dir}/{file}"
-
-        database_name = file[:-4]
-        logical_name_data = f"{database_name}_data"
-        logical_name_log = f"{database_name}_log"
-        data_path = f"{ContainerConfig.data_dir}/{database_name}.mdf"
-        log_path = f"{ContainerConfig.data_dir}/{database_name}_log.ldf"
-
-        sql_command = SQL_COMMAND.format(
-            DB_NAME=database_name,
-            BACKUP_PATH=backup_path,
-            LOGICAL_NAME_DATA=logical_name_data,
-            DATA_PATH=data_path,
-            LOGICAL_NAME_LOG=logical_name_log,
-            LOG_PATH=log_path,
-        )
-
-        # Ejecutar el comando SQL en el contenedor
+    run_app()
 
 
 if __name__ == "__main__":
