7fceb9da30fc497e0494c4461374d9a798d11693
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-05 22:37:42 -0600

    ✨ (utilizar click para agregar comandos): Agregar comandos con click para mejorar la aplicacion de consola y que tenga mas opciones

--- src/mssql_backups/cli.py
+++ src/mssql_backups/cli.py
@@ -1,9 +1,43 @@
-from mssql_backups.service.app_service import run_app
+import click
+
+from mssql_backups.service.list_app import list_app
+from mssql_backups.service.restore_app import restore_app
+from mssql_backups.utils.colors import fore_green
 
 
-def main():
-    run_app()
+@click.group()
+def cli():
+    """CLI interactivo para MSSQL Server
+
+    Herramienta para resturar bases de datos usando archivos .bak, tambien permite
+    crear backups de bases de datos."""
+    pass
+
+
+@cli.command()
+def restore():
+    """Restaurar bases de datos, sigue las instrucciones del asistente"""
+    restore_app()
+
+
+@cli.command()
+def backup_dialog():
+    """Crear backups de bases de datos de forma interactiva"""
+    pass
+
+
+@cli.command()
+def backup():
+    """Crear backups de bases de datos usando parametros"""
+    pass
+
+
+@cli.command()
+def list():
+    """Listar bases de datos disponibles"""
+    list_app()
 
 
 if __name__ == "__main__":
-    main()
+    print(fore_green(f"{'MSSQL Backups':-^60}"))
+    cli()
