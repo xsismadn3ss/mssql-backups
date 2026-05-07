038868df3018670dd4a15bf9c639c83d6912782f
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-07 13:41:43 -0600

    ✨ Agregar `comando config status`
    Con este comando se puede ver de forma rapida todas las configuraciones que estan guardadas en cache

--- src/mssql_backups/service/config/config.py
+++ src/mssql_backups/service/config/config.py
@@ -5,8 +5,21 @@ import typer
 from .bak import app as bak_app
 from .conn import app as conn_app
 
-
 app = typer.Typer(name="config", help="Administrar configuraciones guardadas en SQLite")
 
 app.add_typer(bak_app, name="bak")
 app.add_typer(conn_app, name="conn")
+
+
+@app.command()
+def status():
+    """
+    Mostrar estatus de las configuraciones guardars. Utiliza este comando para ver de forma
+    rapida el listado de configuracion guardadas en cache.
+    """
+    from .bak import ls as bak_ls
+    from .conn import ls as conn_ls
+
+    conn_ls()
+    print()
+    bak_ls()
