723f17bf52091d7ce2b09e9a75a9021d0cd7de42
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-07 08:55:05 -0600

    🎨 Aplicar formato al codigo

--- src/mssql_backups/service/restore/config.py
+++ src/mssql_backups/service/restore/config.py
@@ -5,8 +5,10 @@ import typer
 from .begin import begin as begin_command
 from .files import files as files_command
 
-
-app = typer.Typer(name="restore", help="Restaurar bases de datos usando configuraciones guardadas en SQLite")
+app = typer.Typer(
+    name="restore",
+    help="Restaurar bases de datos usando configuraciones guardadas en caché",
+)
 
 app.command(name="files")(files_command)
 app.command(name="begin")(begin_command)
