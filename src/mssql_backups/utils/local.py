77c5c1152fbab0f30c83fe261b583f4a5715414e
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-06 18:36:42 -0600

    ✨ Definir utilidades para cache local
    utilizar sqlmodel para manipular la base de datos sqlite para manejar los datos de configuraciones

---
+++ src/mssql_backups/utils/local.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from sqlmodel import SQLModel, create_engine
+
+
+def get_db_path():
+    base_dir = Path.home() / ".mssql-bakups"
+    base_dir.mkdir(parents=True, exist_ok=True)
+    db_path = base_dir / "config.db"
+    return db_path
+
+
+def get_engine():
+    # Base de datos local con sqlite
+    engine = create_engine(f"sqlite:///{get_db_path()}")
+    return engine
+
+
+def create_tables(engine):
+    from mssql_backups.models import tables
+
+    SQLModel.metadata.create_all(engine)
