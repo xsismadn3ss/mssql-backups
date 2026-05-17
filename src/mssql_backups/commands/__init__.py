04bb84081521c47f4514f0fe414d46fe98199610
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-17 14:40:13 -0600

    ✨ Implementar comando para versión del proyecto
    Ahora se puede consultar la versión del proyecto

---
+++ src/mssql_backups/commands/__init__.py
@@ -0,0 +1,26 @@
+from .bak import app as bak
+from .cache import app as cache
+from .config import app as config
+from .db import app as db
+from .restore import app as restore
+
+
+def commands_dict():
+    """Listado de comandos en formato de diccionario"""
+    return {
+        'bak': bak,
+        'cache': cache,
+        'config': config,
+        'db': db,
+        'restore': restore,
+    }
+
+
+__all__ = [
+    'bak',
+    'cache',
+    'config',
+    'db',
+    'restore',
+    'commands_dict',
+]
---
+++ src/mssql_backups/config.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class AppConfig:
+    version: str = "3.14.0"
+    name: str = "MSSQL Backups"
